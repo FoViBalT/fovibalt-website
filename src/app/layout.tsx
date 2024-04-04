f8ec886b1987f65fc1a2d857735333f1b8fa3c48
DaminikDavydau <dominikdavydov3d@gmail.com>
2024-04-04 23:19:18 +0300

    fix: import open sans fonts from google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     </head>
       <body className={FuturaDemiC.className}>{children}</body>
