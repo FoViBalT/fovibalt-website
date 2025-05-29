90ef781e9bde53bdcc7b90356e39a13c4a9e3346
Nikita <nikita.klepikovs.d@gmail.com>
2025-05-29 14:25:52 +0300

    added redirect on personal area button

--- src/components/parts/Button.tsx
+++ src/components/parts/Button.tsx
@@ -4,19 +4,20 @@ interface ButtonProps {
     text: string,
     type?: string,
     marginTop?: number
+    onClickL?: () => void
 }
 
-export default function Button({ text, type, marginTop=0 }: ButtonProps) {
+export default function Button({ text, type, marginTop=0, onClickL}: ButtonProps) {
     if (type === undefined || type === "small") {
         return (
-            <button className={styles.buttonSmall}>
+            <button className={styles.buttonSmall} onClick={onClickL}>
                 {text}
             </button>
         )
     }
     else if (type == "big") {
         return (
-            <button style={{marginTop: marginTop + 'px'}} className={styles.buttonBig}>
+            <button style={{marginTop: marginTop + 'px'}} className={styles.buttonBig} onClick={onClickL}>
                 {text}
             </button>
         )
--- src/components/parts/Header.tsx
+++ src/components/parts/Header.tsx
@@ -1,13 +1,18 @@
+'use client';
 import Button from "./Button"
 
 import styles from "./Header.module.css";
 
 export default function Header() {
+     const handlePersonalAreaClick = () => {
+        window.location.href = 'https://dashboard.fovibalt.com'; // Redirect to the personal area
+    };
+
     return (
         <header className={styles.header}>
             <Button text="About fovilight"></Button>
             <Button text="Shop"></Button>
-            <Button text="Personal Area"></Button>
+            <Button text="Personal Area" onClickL={handlePersonalAreaClick}></Button>
         </header>
-    )
+    );
 }
