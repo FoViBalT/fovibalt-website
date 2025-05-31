8f0f8a36ee431023ce20017350a98bd77f4aa0b8
Nikita <62812039+sosiskus@users.noreply.github.com>
2025-05-31 20:39:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    /* UNOPTIMIZED JUST FOR TEST IN FUTURE REMOVE IT */
 images: { unoptimized: true },
 }
 module.exports = nextConfig
